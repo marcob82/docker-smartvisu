78a1a76e17bff9356d66fa109636e3c3315594a4
Foxi352 <serge@swa.lu>
2017-04-22 22:26:53 +0200

    First develop branch commit

--- build-smartvisu.sh
+++ build-smartvisu.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-    VERSION=2.8
-else
-    VERSION=$1
-fi
-
-docker build --rm --build-arg SHNG_SMARTVISU_BRANCH=v$VERSION -t foxi352/smartvisu:$VERSION --file Dockerfile.smartvisu .
+docker build --rm --build-arg SHNG_SMARTVISU_BRANCH=develop -t foxi352/smartvisu:develop --file Dockerfile.smartvisu .
